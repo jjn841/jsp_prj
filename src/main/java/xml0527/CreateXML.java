8f83bfe880d505e2db3a7e750a404a054f877104
jang <nyeong1236@gamail.com>
2025-05-27 12:33:53 +0900

    12:30분 커밋

--- src/main/java/xml0527/CreateXML.java
+++ src/main/java/xml0527/CreateXML.java
@@ -1,10 +1,54 @@
 package xml0527;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
+
 public class CreateXML {
+	
+	public void creatXML() {
+		//1.XML문서객체 생성
+		 Document doc=new Document();
+		 
+		 //2.최상위 부모노드 생성
+		 Element rootNode=new Element("root");
+		 
+		 //3.자식 노드 생성
+		 Element msgNode=new Element("msg");
+		 //자식노드에 값 설정
+		 //System.out.println( msgNode );
+		 msgNode.setText("안녕하세요?");
+		 
+		 //자식노드를 부모노드에 배치
+		 rootNode.addContent( msgNode ); 
+		 //모든 자식노드를 가진 부모노드를 문서객체에 배치
+		 doc.addContent(rootNode);
+		 
+		 //출력객체 생성
+		 //XMLOutputter xOut=new XMLOutputter(Format.getRawFormat());
+		 //XMLOutputter xOut=new XMLOutputter(Format.getCompactFormat());
+		 XMLOutputter xOut=new XMLOutputter(Format.getPrettyFormat());
 
+		 //콘솔출력
+			 try {
+				//콘솔출력
+				xOut.output(doc, System.out);
+				//파일로 출력
+				xOut.output( doc, new FileOutputStream("C:/dev/workspace/jsp_prj/src/main/webapp/xml0527/create.xml"));
+				
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}//end catch
+		 
+	}//creatXML
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new CreateXML().creatXML();
+	}//main
 
-	}
-
-}
+}//class
